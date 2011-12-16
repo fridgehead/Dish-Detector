755feca9786de747a48f960cbf3c3f86d78cd361
tom <tom@imakethin.gs>
2011-12-16 23:13:25 +0000

    added bg diffs = seems better

--- working.py
+++ working.py
@@ -4,15 +4,22 @@ import numpy as np
 
 def main(thresh):
   im = cv.LoadImage(sys.argv[2])
+  bg = cv.LoadImage("test3.jpg")
+  graybg = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
   gray = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
   edges = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_8U, 1)
 
+
+  cv.CvtColor(bg, graybg, cv.CV_BGR2GRAY)
+
   cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
+  cv.Sub(gray, graybg, gray)
+  cv.EqualizeHist(gray,gray)
   cv.Canny(gray, edges, thresh, thresh / 2, 3)
-  cv.Smooth(gray, gray, cv.CV_GAUSSIAN, 3, 3) 
+  cv.Smooth(edges, edges, cv.CV_GAUSSIAN, 3, 3) 
 
   storage = cv.CreateMat(640, 1, cv.CV_32FC3)
-  cv.HoughCircles(gray, storage, cv.CV_HOUGH_GRADIENT, 2, gray.width / 18, thresh, 200,0,0)
+  cv.HoughCircles(edges, storage, cv.CV_HOUGH_GRADIENT, 3, gray.width / 18, thresh, 200,0,0)
 
   for i in range(storage.rows ):
     val = storage[i, 0] 
@@ -23,6 +30,7 @@ def main(thresh):
     print (radius, center)
 
     cv.Circle(im, center, radius, (0, 0, 255), 3, 8, 0)
+    cv.Circle(edges, center, radius, (0, 0, 255), 3, 8, 0)
 
 
 
